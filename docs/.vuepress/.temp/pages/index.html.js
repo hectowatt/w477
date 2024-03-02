4e732e24c0bb71868fdfa8fc3a3979543f01335d
hectowatt <w477.blog@gmail.com>
2024-03-02 16:15:57 +0900

    メアド変更

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/w477/dev/w477/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"w477\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"このブログについて\",\"slug\":\"このブログについて\",\"link\":\"#このブログについて\",\"children\":[]},{\"level\":2,\"title\":\"GitHub Pagesへ移行した理由\",\"slug\":\"github-pagesへ移行した理由\",\"link\":\"#github-pagesへ移行した理由\",\"children\":[]}],\"git\":{\"updatedTime\":1709122721000,\"contributors\":[{\"name\":\"hectowatt\",\"email\":\"0903tamaki@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"w477\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"このブログについて\",\"slug\":\"このブログについて\",\"link\":\"#このブログについて\",\"children\":[]},{\"level\":2,\"title\":\"GitHub Pagesへ移行した理由\",\"slug\":\"github-pagesへ移行した理由\",\"link\":\"#github-pagesへ移行した理由\",\"children\":[]}],\"git\":{\"updatedTime\":1709122721000,\"contributors\":[{\"name\":\"hectowatt\",\"email\":\"w477.blog@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
