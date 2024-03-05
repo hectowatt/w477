66f564c5c63b5096f13f32a139aab985586684cd
hectowatt <w477.blog@gmail.com>
2024-03-05 21:19:48 +0900

    faviconの表示できた

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"en-US\",\"title\":\"\",\"description\":\"\",\"head\":[],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"ja-JP\",\"title\":\"w477\",\"description\":\"\",\"head\":[[\"link\",{\"rel\":\"icon\",\"type\":\"image/png\",\"href\":\"/favicon.png\"}]],\"locales\":{}}")
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,4 +5,10 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
+  lang: 'ja-JP',
+  title: 'w477',
+  head:[
+    ['link',{rel:'icon',type:'image/png',href:'/favicon.png'}]
+  ]
 })
+
