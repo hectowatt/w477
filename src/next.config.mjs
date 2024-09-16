f2f578a36bccb03f657c743bde30cdf5979f7184
hectowatt <0903tamaki@gmail.com>
2024-09-16 10:34:37 +0900

    github pagesの設定を反映

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -2,9 +2,6 @@
 const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
 
 const nextConfig = {
-    reactStrictMode: true,
-    assetPrefix: branchName,
-    basePath: branchName,
     output: "export"
 };
 
