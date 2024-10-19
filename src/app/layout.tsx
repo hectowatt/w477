0ec8071a7ceef37f04b4dc2905ef14c0ed11bdfa
hectowatt <0903tamaki@gmail.com>
2024-10-19 11:45:16 +0000

    ヘッダーとフッターを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,40 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className={inter.className}>
+        {children}
+        <Header />
+        <Hero></Hero>
+        <Footer />
+      </body>
     </html>
+  );
+}
+
+function Header() {
+  return (
+    <header className="text-white p-4">
+      <h1>w477 Blog</h1>
+    </header>
+  );
+}
+
+function Hero() {
+  return (
+    <div
+      className="bg-cover bg-center h-64 flex items-center justify-center"
+      style={{ backgroundImage: "url('/hero-image.jpg')" }}
+    >
+      <h2 className="text-white text-4xl font-bold">Welcome to My Site</h2>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-8">
+      <p>© 2024 w477</p>
+    </footer>
   );
 }
