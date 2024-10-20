71cf2763419581368c68e51258894394257a6e23
hectowatt <0903tamaki@gmail.com>
2024-10-20 11:43:57 +0000

    ヘッダーとフッターとヒーローセクションを修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,30 @@
 import type { Metadata } from "next";
 import "/globals.css";
 import { ReactNode } from "react";
-import getConfig from 'next/config';
+import getConfig from "next/config";
 
 const { publicRuntimeConfig } = getConfig();
-const basePath = publicRuntimeConfig.basePath || '';
+const basePath = publicRuntimeConfig.basePath || "";
 
-type LayoutProps ={
+type LayoutProps = {
   children: ReactNode;
-}
+};
 
 export const metadata: Metadata = {
   title: "w477",
   description: "My blog by Next.js",
+  icons: {
+    icon: "/favicon.ico", // favicon.icoのパス
+    apple: "/apple-touch-icon.png", // Apple向けアイコン
+  },
 };
 
-export default function RootLayout({children } : LayoutProps){
+export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="ja" className="h-full">
       <body className="h-full flex flex-col min-h-screen">
         <Header />
+        <Hero/>
         <main className="flex-grow">{children}</main>
         <Footer />
       </body>
@@ -29,11 +34,16 @@ export default function RootLayout({children } : LayoutProps){
 
 function Header() {
   return (
-    <header className="text-white p-4 text-2xl">
+    <header className="text-black p-4 text-2xl">
       <div className="flex gap-4 items-center">
-      <a href="https://hectowatt.github.io/w477/" target="_blank" rel="noopener noreferrer">
-      <img src={`${basePath}/w477.png`}></img></a>
-      <h1>w477 Blog</h1>
+        <a
+          href="https://hectowatt.github.io/w477/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={`${basePath}/w477.png`}></img>
+        </a>
+        <h1>w477 Blog</h1>
       </div>
     </header>
   );
@@ -41,11 +51,7 @@ function Header() {
 
 function Hero() {
   return (
-    <div
-      className="bg-cover bg-center h-64 flex items-center justify-center"
-      style={{ backgroundImage: "url('/hero-image.jpg')" }}
-    >
-      <h2 className="text-white text-4xl font-bold">Welcome to My Site</h2>
+    <div className="hero-image">
     </div>
   );
 }
@@ -54,11 +60,29 @@ function Footer() {
   return (
     <footer className="text-black p-4 mt-8">
       <p>w477 Powered by Next.js</p>
-      <div className="flex gap-4 mi-t" >
-      <a href="https://misskey.io/@sublimesab" target="_blank" rel="noopener noreferrer">
-      <img src={`${basePath}/MisskeyIcon.png`} alt="Misskey" className="hover:opacity-80 transition-opacity duration-300"></img></a>
-      <a href="https://github.com/hectowatt" target="_blank" rel="noopener noreferrer">
-      <img src={`${basePath}/github.png`} alt="Misskey" className="hover:opacity-80 transition-opacity duration-300"></img></a>
+      <div className="flex gap-4 mi-t">
+        <a
+          href="https://misskey.io/@sublimesab"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={`${basePath}/MisskeyIcon.png`}
+            alt="Misskey"
+            className="hover:opacity-80 transition-opacity duration-300"
+          ></img>
+        </a>
+        <a
+          href="https://github.com/hectowatt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={`${basePath}/github.png`}
+            alt="Misskey"
+            className="hover:opacity-80 transition-opacity duration-300"
+          ></img>
+        </a>
       </div>
     </footer>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import basePath from "../../next.config.mjs"
-const BASE_PATH = basePath ? basePath : ""
 
 export default function Home() {
   return (
-    <main >
+    <main>
 
     </main>
   );
