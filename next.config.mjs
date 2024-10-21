9bb563ecfe4827337c4e36431df93ea5bdabb4f1
hectowatt <0903tamaki@gmail.com>
2024-10-21 13:52:49 +0000

    assertPrefixを移動

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,9 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: "export",
     basePath: isProd ? '/w477' : '',  // 本番環境だけ適用
+    assetPrefix: isProd ? '/w477/' : '',
     publicRuntimeConfig: {
         basePath: isProd ? '/w477' : '', // 同じbasePathを公開設定に含める
-        assetPrefix: isProd ? '/w477/' : '',
       },
     images: {
         unoptimized: true,
