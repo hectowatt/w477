50aa35e4aadba7cd18390555682e76e6294503a9
hectowatt <0903tamaki@gmail.com>
2025-01-13 12:55:03 +0000

    sitemap.config.jsにbasePathを反映

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,5 +4,6 @@ module.exports = {
     generateRobotsTxt: true, // robots.txt も生成
     generateIndexSitemap: false, // インデックス用サイトマップを生成しない
     outDir: './out', // 出力先を指定 (GitHub Pages 用に調整)
+    basePath: '/w477', // リポジトリ名を設定
   };
   
