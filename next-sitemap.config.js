b7420cff211107f7eff25f1d5d864d817740dd96
hectowatt <0903tamaki@gmail.com>
2025-01-13 12:40:27 +0000

    sitemap.config.jsを修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://<your-github-username>.github.io/<repository-name>', // GitHub Pages の URL
+    siteUrl: 'https://hectowatt.github.io/w477', // GitHub Pages の URL
     generateRobotsTxt: true, // robots.txt も生成
     generateIndexSitemap: false, // インデックス用サイトマップを生成しない
     outDir: './out', // 出力先を指定 (GitHub Pages 用に調整)
