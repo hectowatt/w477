fb66f8c929bdf522b083957e302f4ad89e550110
hectowatt <w477.dev@gmail.com>
2025-06-18 03:21:35 +0000

    faviconのサイズを48に変更

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: 'https://hectowatt.github.io/w477', // GitHub Pages の URL
   generateRobotsTxt: true, // robots.txt も生成
   generateIndexSitemap: false, // インデックス用サイトマップを生成しない
-  outDir: './public', // 出力先を指定 (GitHub Pages 用に調整)
+  outDir: './out', // 出力先を指定 (GitHub Pages 用に調整)
   basePath: '/w477', // リポジトリ名を設定
   autoLastmod: false,
   transform: async (config, path) => {
